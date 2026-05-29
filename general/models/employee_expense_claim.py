4a2cea7b81aafed2741bca5e7f3729c35ab335e2
K.Praveen <k.praveen@fingertipplus.com>
2026-05-29 12:07:56 +0530

    Expense claim and Expense Lines Changes Updated

--- general/models/employee_expense_claim.py
+++ general/models/employee_expense_claim.py
@@ -82,6 +82,7 @@ class EmployeeExpenseClaim(models.Model):
         ('claim_number_uniq', 'unique(claim_number, company_id)',
          'Claim number must be unique per company.'),
     ]
+    bill_available = fields.Boolean(string="Bills Available")
 
     # --------------------------------------------------------------
     @api.model_create_multi
